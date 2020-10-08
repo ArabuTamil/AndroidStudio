67d811e6b74ccccdf4be40f37556d5ad0a62c9d7
Mohamed Ibrahim Ansari <54184745+ibrahimansar@users.noreply.github.com>
2020-10-08 10:42:56 +0530

    .

---
+++ app/src/main/java/com/example/arabutamilkeyboard/ArabuTamil.java
@@ -0,0 +1,57 @@
+package com.example.arabutamilkeyboard;
+
+import android.app.Service;
+import android.content.Intent;
+import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+import android.os.IBinder;
+
+public class ArabuTamil extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
+
+    private KeyboardView kv;
+    private Keyboard Keyboard;
+
+    private boolean isCaps = false;
+    
+
+    @Override
+    public void onPress(int primaryCode) {
+
+    }
+
+    @Override
+    public void onRelease(int primaryCode) {
+
+    }
+
+    @Override
+    public void onKey(int primaryCode, int[] keyCodes) {
+
+    }
+
+    @Override
+    public void onText(CharSequence text) {
+
+    }
+
+    @Override
+    public void swipeLeft() {
+
+    }
+
+    @Override
+    public void swipeRight() {
+
+    }
+
+    @Override
+    public void swipeDown() {
+
+    }
+
+    @Override
+    public void swipeUp() {
+
+    }
+}
